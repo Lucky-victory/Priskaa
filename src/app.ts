31b57ae80cea6ac010edd3bd06eeb874a148acbc
omolara5861 <debbiegterra@gmail.com>
2024-03-13 06:28:02 +0100

    feat: :sparkles: Added ejs

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
-
+import ejs from "ejs";
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
 // Routes
 import { index } from "./routes/index";
-// Create Express server
+
 export const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3800);
+
 app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
+
+app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 
